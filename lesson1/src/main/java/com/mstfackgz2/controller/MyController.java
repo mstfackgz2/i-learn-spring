631e697a0edb1f847fcada1131dc2b4bf27b1661
mstfackgz2 <114994317+mstfackgz2@users.noreply.github.com>
2024-08-26 12:35:08 +0300

    Adding to Controler

--- lesson1/src/main/java/com/mstfackgz2/controller/MyController.java
+++ lesson1/src/main/java/com/mstfackgz2/controller/MyController.java
@@ -1,11 +1,35 @@
 package com.mstfackgz2.controller;
 
+import com.mstfackgz2.model.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MyController {
 
+    @GetMapping("/header")
+    public String getHeader(@RequestHeader("My-Header") String myHeader){//gelen istegin icersinde My-Header isminde bir bilgi yoksa bad request doner
+        return "your header" + myHeader;
+    }
+
+
+    // request body json verilerini dogrudan bir nesneye esitler
+    @PostMapping("users")
+    public User saveUser(@RequestBody User user){
+        user.setPassword("");
+        System.out.println("user saved");
+        return user;
+    }
+
+    // request body json verilerini dogrudan bir nesneye esitler
+    @PostMapping("users-all")
+    public List<User> saveAllUser(@RequestBody List<User> users){
+        users.forEach(user -> user.setPassword(""));
+        System.out.println("users saved");
+        return users;
+    }
 
     //@GetMapping(path = "/message1/{message}") // tek seferli icin kullanılabılır
     @GetMapping({"/message1","/message1/{message}"}) // bu sekılde de mesaj gelmesede calısacak sekılde ayarladık
@@ -14,8 +38,10 @@ public class MyController {
         return "Your Message is: " + message;
     }
 
-    @GetMapping(path = "/message2")
-    public String getMyMessage2(@RequestParam(name = "message") String message) {//message2?message= den sonra gelenler parametre olarak kabul edılır
+    @GetMapping(path = "/message2") //
+    public String getMyMessage2(@RequestParam(name = "message",//message2?message= den sonra gelenler parametre olarak kabul edılır
+                            required = false,
+                            defaultValue = "default messagge") String message) {
         return "Your Message is: " + message;
     }
 
